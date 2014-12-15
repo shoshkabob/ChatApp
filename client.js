7ce214bd7730c3c7870c6755e4e67ce29c2b967a
Shoshanah Weisinger <shoshcw@gmail.com>
2014-12-15 10:28:50 -0500

    initial ChatApp commit

---
+++ client.js
@@ -0,0 +1,75 @@
+var client = new WebSocket("ws://localhost:3000");
+
+var usertext = document.querySelector("#usertext");
+var chat = document.querySelector(".chat");
+var send = document.querySelector("#send");
+var username = window.prompt("What is your name?");
+var colors = [];
+var x = 0;
+
+function User() {
+	this.name = username;
+	this.generateColor = function() {
+		var newColor = "rgb(" + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ")";
+		colors.unshift(newColor);
+		this.color = colors[0];
+	}
+}
+
+client.addEventListener("open", function () {
+	var currentUser = new User;
+	currentUser.generateColor();
+
+	function sendMessage() {
+		var text = usertext.value;
+		var msg = { name: currentUser.name, message: text, color: currentUser.color };
+		var sendMsg = JSON.stringify(msg);
+		client.send(sendMsg);
+	}
+
+	send.addEventListener("click", sendMessage);
+	usertext.addEventListener("keypress", function (evt) {
+		if (evt.keyCode === 13) {
+			sendMessage();
+		}	
+	});
+
+	client.addEventListener("message", function (stuff) {
+		var txt = JSON.parse(stuff.data);
+		var msg = txt.message;
+		var chatBlurb = document.createElement("p");
+		chatBlurb.setAttribute("class", "user");
+		chatBlurb.style.color = txt.color;
+		chat.appendChild(chatBlurb);
+		var chatUser = document.createElement("b");
+		chatUser.setAttribute("class", "uname");
+		chatUser.innerHTML = txt.name + ": ";
+		var chatMsg = document.createElement("a");
+		chatMsg.setAttribute("class", "umsg");
+		chatMsg.innerHTML = msg;
+		chatBlurb.appendChild(chatUser);
+		chatBlurb.appendChild(chatMsg);
+		if (msg.substr(0, 7) === "http://") {
+			chatMsg.setAttribute("href", msg);
+		}
+		if (msg.substr(-3) === "png" || msg.substr(-3) === 
+			"jpg" || msg.substr(-4) === "jpeg" || msg.substr(-4) === "tiff" || msg.substr(-3) === "gif") {
+			var newline = document.createElement("br");
+			chatBlurb.appendChild(newline);
+			var chatImg = document.createElement("img");
+			chatImg.setAttribute("src", msg);
+			chatBlurb.appendChild(chatImg);
+		}
+		if (txt.name === currentUser.name) {
+			chatBlurb.style.background = "lightblue";
+		}
+
+		usertext.value = "";
+		if (txt.name === currentUser.name && msg === "**this user has been banned**") {
+			window.alert("Sorry, you used a banned word. You have been booted from the chat.");
+		}
+	});
+});
+
+
+
---
+++ server.js
@@ -0,0 +1,70 @@
+var WSS = require("ws").Server;
+var server = new WSS({port: 3000});
+var notifier = require("node-notifier");
+var users = [];
+var clients = [];
+var msgHist = [];
+var x = 0;
+
+server.on("connection", function (conn) {
+
+	clients.push(conn);
+
+	console.log(clients.length + " users are in the chat.");
+
+	msgHist.forEach(function (msg) {
+		conn.send(msg);
+	});
+
+	
+
+	conn.on("message", function (input) {
+		var info = JSON.parse(input);
+		var message = info.name + ": " + info.message;
+		var toSend = {name: info.name, message: info.message, color: info.color };
+		function sending() {
+			var sendMsg = JSON.stringify(toSend);
+			clients.forEach(function (cl) {
+				cl.send(sendMsg);
+			});
+			msgHist.push(JSON.stringify(toSend));
+		}
+		var arr = info.message.split(" ");
+		console.log(message);
+		if(arr.indexOf("kike") > -1 || arr.indexOf("kikes") > -1) {
+			toSend.message = "**this user has been banned**";
+			sending();
+			conn.close();
+		}
+		else if (info.message === "(table flip)"){
+			toSend.message = "(╯°□°）╯︵ ┻━┻";
+			sending();
+		}
+		else if (info.message === "(shrug)") {
+			toSend.message = "¯\\_(ツ)_/¯";
+			sending();
+		}
+		else if (info.message === "(flower in hair)") {
+			toSend.message = "(◡ ‿ ◡ ✿)";
+			sending();
+		}
+		else if (arr[0] === "/yell") {
+			arr.shift("/yell");
+			var newMessage = arr.join(" ");
+			toSend.message = newMessage.toUpperCase();
+			sending();
+		}
+		else {
+			toSend.message = info.message;
+			sending();
+		}
+		
+		
+		
+	});
+
+	conn.on("close", function () {
+		clients.splice(clients.indexOf(this), 1);
+		console.log(clients.length + " users are in the chat.");
+	});
+});
