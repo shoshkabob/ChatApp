6a83afcea3646823657b945e09fa72c66e0deaa3
Shoshanah Weisinger <shoshcw@gmail.com>
2015-01-02 01:13:08 -0500

    1st attempt at online user list

--- client.js
+++ client.js
@@ -1,74 +1,149 @@
 var client = new WebSocket("ws://shoshanah.princesspeach.nyc:3000");
 
 var usertext = document.querySelector("#usertext");
+var userInput = usertext.value;
 var chat = document.querySelector(".chat");
 var send = document.querySelector("#send");
-var username = window.prompt("What is your name?");
+var username = document.querySelector("#username");
+var online = document.querySelector(".online");
+var usersOnline = [];
+var formerUserName = "";
 var colors = [];
-var users = [];
 var x = 0;
 
 function User() {
-	this.name = username;
+	this.name = "Anonymous";
+	this.conn = client;
 	this.generateColor = function() {
 		var newColor = "rgb(" + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ", " + Math.floor(Math.random()*255) + ")";
 		colors.unshift(newColor);
 		this.color = colors[0];
 	}
-	users.push(this);
 }
 
 client.addEventListener("open", function () {
 	var currentUser = new User;
 	currentUser.generateColor();
 
-	function sendMessage() {
-		var text = usertext.value;
-		var msg = { name: currentUser.name, message: text, color: currentUser.color };
+	usersOnline.forEach(function (elem) {
+		var thisUser = document.createElement("li");
+		thisUser.innerHTML = elem.name;
+
+	});
+
+	function sendMessage(input) {
+		var msg = { name: currentUser.name, message: input, color: currentUser.color, conn: currentUser.conn };
 		var sendMsg = JSON.stringify(msg);
 		client.send(sendMsg);
 	}
 
-	send.addEventListener("click", sendMessage);
+	sendMessage(null);
+
+	username.innerHTML = "Anonymous";
+
+	username.addEventListener("dblclick", function () {
+		var userChange = document.createElement("input");
+		userChange.setAttribute("type", "text");
+		var sidebar = document.querySelector(".sidebar");
+		sidebar.replaceChild(username, userChange);
+		userChange.innerHTML = username.innerHTML;
+		userChange.addEventListener("keypress", function (evt) {
+			if (evt.keyCode === 13) {
+				formerUserName = currentUser.name;
+				username.innerHTML = userChange.value;
+				var announcement = formerUserName + " has changed name to " + userChange.value;
+				console.log(announcement);
+				client.send(announcement);
+				currentUser.name = userChange.value;
+				sidebar.replaceChild(userChange, username);
+				sendMessage();
+			}
+		});
+	});
+
+	send.addEventListener("click", sendMessage(userInput));
 	usertext.addEventListener("keypress", function (evt) {
 		if (evt.keyCode === 13) {
-			sendMessage();
+			sendMessage(userInput);
 		}	
 	});
 
 	client.addEventListener("message", function (stuff) {
 		var txt = JSON.parse(stuff.data);
-		var msg = txt.message;
-		var chatBlurb = document.createElement("div");
-		chatBlurb.setAttribute("class", "user");
-		chatBlurb.style.color = txt.color;
-		chat.appendChild(chatBlurb);
-		var chatUser = document.createElement("p");
-		chatUser.setAttribute("class", "uname");
-		chatUser.innerHTML = txt.name + ": ";
-		var chatMsg = document.createElement("p");
-		chatMsg.setAttribute("class", "umsg");
-		chatMsg.innerHTML = " " + msg;
-		chatBlurb.appendChild(chatUser);
-		chatBlurb.appendChild(chatMsg);
-		if (msg.substr(0, 7) === "http://") {
-			chatMsg.setAttribute("href", msg);
-		}
-		if (msg.substr(-3) === "png" || msg.substr(-3) === 
-			"jpg" || msg.substr(-4) === "jpeg" || msg.substr(-4) === "tiff" || msg.substr(-3) === "gif") {
-			var newline = document.createElement("br");
-			chatBlurb.appendChild(newline);
-			var chatImg = document.createElement("img");
-			chatImg.setAttribute("src", msg);
-			chatBlurb.appendChild(chatImg);
-		}
-		if (txt.name === currentUser.name) {
-			chatBlurb.style.background = "lightblue";
-		}
+		if (typeof txt === 'object' && txt.hasOwnProperty(message)) {
+			var msg = txt.message;
+			if (currentUser.name !== txt.name && usersOnline.indexOf(txt.name) > -1) {
+				var onlineUser = { name: txt.name, color: txt.color, conn: txt.conn };
+				usersOnline.forEach(function (elem) {
+					if (onlineUser.conn === elem.conn) {
+						var printedUsers = document.querySelectorAll("li");
+						for (i = 0; i < printedUsers.length; i++) {
+							var n = printedUser[i];
+							if (n.textContent === elem.name) {
+								elem.name = onlineUser.name;
+								n.innerHTML = elem.name;
+							} 
+						}	
+					}
+					else {
+						usersOnline.push(onlineUser);
+						var thisUser = document.createElement("li");
+						thisUser.innerHTML = onlineUser.name;
+						online.appendChild(thisUser);
+					}
+				});
+			}
+			if (msg !== null) {
+				var chatBlurb = document.createElement("div");
+				chatBlurb.setAttribute("class", "user");
+				chatBlurb.style.color = txt.color;
+				chat.appendChild(chatBlurb);
+				var chatUser = document.createElement("p");
+				chatUser.setAttribute("class", "uname");
+				chatUser.innerHTML = txt.name + ": ";
+				var chatMsg = document.createElement("p");
+				chatMsg.setAttribute("class", "umsg");
+				chatMsg.innerHTML = " " + msg;
+				chatBlurb.appendChild(chatUser);
+				chatBlurb.appendChild(chatMsg);
+				if (msg.substr(0, 7) === "http://") {
+					chatMsg.setAttribute("href", msg);
+				}
+				if (msg.substr(-3) === "png" || msg.substr(-3) === 
+					"jpg" || msg.substr(-4) === "jpeg" || msg.substr(-4) === "tiff" || msg.substr(-3) === "gif") {
+					var newline = document.createElement("br");
+					chatBlurb.appendChild(newline);
+					var chatImg = document.createElement("img");
+					chatImg.setAttribute("src", msg);
+					chatBlurb.appendChild(chatImg);
+				}
+				if (txt.name === currentUser.name) {
+					chatBlurb.style.background = "lightblue";
+				}
 
-		usertext.value = "";
-		if (txt.name === currentUser.name && msg === "**this user has been banned**") {
-			window.alert("Sorry, you used a banned word. You have been booted from the chat.");
+				usertext.value = "";
+				if (txt.name === currentUser.name && msg === "**this user has been banned**") {
+					window.alert("Sorry, you used a banned word. You have been booted from the chat.");
+				}
+			}
+		}
+		else if (typeof txt === 'string') {
+			var words = txt.split(" ");
+			var phrase = [];
+			words.forEach(function (elem) {
+				if (elem === "has" || elem === "changed" || elem === || elem === "name" || elem === "to") {
+					phrase.push(elem);
+				}
+				var checkPhrase = phrase.join(" ");
+				if (checkPhrase === "has changed name to") {
+					var chatBlurb = document.createElement("div");
+					chatBlurb.setAttribute("class", "announce");
+					chat.appendChild(chatBlurb);
+					var announce = document.createElement("p");
+					announce.innerHTML = txt;
+					chatBlurb.appendChild(announce);
+				}
+			});
 		}
 	});
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var WSS = require("ws").Server;
 var server = new WSS({port: 3000});
 var notifier = require("node-notifier");
-var users = [];
 var clients = [];
 var msgHist = [];
 var x = 0;
@@ -16,49 +15,54 @@ server.on("connection", function (conn) {
 		conn.send(msg);
 	});
 
-	
-
 	conn.on("message", function (input) {
 		var info = JSON.parse(input);
-		var message = info.name + ": " + info.message;
-		var toSend = {name: info.name, message: info.message, color: info.color };
-		function sending() {
-			var sendMsg = JSON.stringify(toSend);
-			clients.forEach(function (cl) {
-				cl.send(sendMsg);
-			});
-			msgHist.push(JSON.stringify(toSend));
-		}
-		var arr = info.message.split(" ");
-		console.log(message);
-		if(arr.indexOf("kike") > -1 || arr.indexOf("kikes") > -1) {
-			toSend.message = "**this user has been banned**";
-			sending();
-			conn.close();
-		}
-		else if (info.message === "(table flip)"){
-			toSend.message = "(╯°□°）╯︵ ┻━┻";
-			sending();
-		}
-		else if (info.message === "(shrug)") {
-			toSend.message = "¯\\_(ツ)_/¯";
-			sending();
-		}
-		else if (info.message === "(flower in hair)") {
-			toSend.message = "(◡ ‿ ◡ ✿)";
-			sending();
-		}
-		else if (arr[0] === "/yell") {
-			arr.shift("/yell");
-			var newMessage = arr.join(" ");
-			toSend.message = newMessage.toUpperCase();
-			sending();
+		if (typeof info === 'object') {
+			var message = info.name + ": " + info.message;
+			var toSend = {name: info.name, message: info.message, color: info.color };
+			function sending() {
+				var sendMsg = JSON.stringify(toSend);
+				clients.forEach(function (cl) {
+					cl.send(sendMsg);
+				});
+				msgHist.push(JSON.stringify(toSend));
+			}
+			var arr = info.message.split(" ");
+			console.log(message);
+			if(arr.indexOf("kike") > -1 || arr.indexOf("kikes") > -1) {
+				toSend.message = "**this user has been banned**";
+				sending();
+				conn.close();
+			}
+			else if (info.message === "(table flip)"){
+				toSend.message = "(╯°□°）╯︵ ┻━┻";
+				sending();
+			}
+			else if (info.message === "(shrug)") {
+				toSend.message = "¯\\_(ツ)_/¯";
+				sending();
+			}
+			else if (info.message === "(flower in hair)") {
+				toSend.message = "(◡ ‿ ◡ ✿)";
+				sending();
+			}
+			else if (arr[0] === "/yell") {
+				arr.shift("/yell");
+				var newMessage = arr.join(" ");
+				toSend.message = newMessage.toUpperCase();
+				sending();
+			}
+			else {
+				toSend.message = info.message;
+				sending();
+			}
 		}
 		else {
-			toSend.message = info.message;
-			sending();
+			var toSend = JSON.stringify(info);
+			clients.forEach(function (cl) {
+				cl.send(toSend);
+			});
 		}
-		
 		
 		
 	});
